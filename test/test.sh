46a9ca29a9f948957a55a576d6ce2ab64440599b
Jod Aspnmy <aspnmy@gmail.com>
2025-02-17 13:49:56 -0500

    chore: 修复 .gitignore 规则，确保正确排除日志文件

--- test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -gateway-mac 必须传
-# 如何获取网关mac地址：ip addr show | grep link/ether
-# 或者输入ip addr show，查看你的访问外网的那个网卡的mac地址
-./ollama_scanner-linux-amd64 -gateway-mac "" -input ./ip.txt -output ./custom.csv -no-bench -T 1
